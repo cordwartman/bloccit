0f723cf3ef8df04a3ab1e094d95b3e2a960fefb6
Cord Wartman <cordwartman@gmail.com>
2017-02-08 14:06:26 +0000

    created factories for comment and vote, added a helper to display a message in no user activity, and created a favorite posts for user section

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  def no_posts?(user)
+    if user.posts.count == 0
+      return true
+    end
+  end
+  
+  def no_comments?(user)
+    if user.comments.count == 0
+      return true
+    end
+  end
+  
+   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,9 @@ class User < ActiveRecord::Base
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
+  
+  def has_favorites?
+    favorites.count > 0
+  end
+  
 end
